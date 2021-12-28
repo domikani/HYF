ca287e1132e5b93af8fb431902d25577d5bb794d
domna <domnakanari@gmail.com>
2021-12-28 19:47:59 +0000

    add extra useFormalName parameter and check the behaviour of the function when it is called with empty strings

--- JavaScript1/Week2/flightbooking.js
+++ JavaScript1/Week2/flightbooking.js
@@ -1,18 +1,34 @@
-const getFullName = (firstname,lastname)=>{
+const getFullName = (firstname,lastname, useFormalName=false)=>{
+    
+    
+    if(firstname != null || lastname != null)
+    {
     const input = `${firstname} ${lastname}`
     const inputArray = input.split(" ");
-    console.log(inputArray);
+    //console.log(inputArray);
     for (let letter=0; letter<inputArray.length; letter++){
        
 
         inputArray[letter] = inputArray[letter].charAt(0).toUpperCase() + inputArray[letter].slice(1).toLowerCase();
         
     }
-
     const fullname = inputArray.join(" ");
-    //const firstName = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
-    //const lastName = lastnamecharAt(0).toUpperCase() + lastname.slice.toLowerCase
-    return fullname;
+    const firstName = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
+    const lastName = lastname.charAt(0).toUpperCase() + lastname.slice.toLowerCase
+        if(useFormalName){
+            return `Lord ${fullname}`;
+        }else
+        {
+            return fullname;
+        }
+    }else 
+    {
+        return `Please fill in the form with your details!`
+    }  
 }
 
-console.log(getFullName("doMNa", "kanaRI"));
+const fullname1 = getFullName("domna","kanaRi", false);
+const fullname2 = getFullName("nikolas","chatzinikolaou", true);
+const fullname3 = getFullName();
+
+console.log(`${fullname1}\n${fullname2}\n${fullname3}`);
