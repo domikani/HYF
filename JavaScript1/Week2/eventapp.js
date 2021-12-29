f4c70f83faef9b04cf986b749d0cfeff6482485f
domna <domnakanari@gmail.com>
2021-12-29 20:13:17 +0000

    initial function creation

---
+++ JavaScript1/Week2/eventapp.js
@@ -0,0 +1,13 @@
+const getEventWeekDay = (days)=>
+{
+    const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const today = new Date();
+    const dayOfTheWeekNumeric = today.getDay();
+    const eventDaysFromToday = days + dayOfTheWeekNumeric;
+    const eventDayNumeric = (eventDaysFromToday % weekDays.length);
+    const eventDay = weekDays[eventDayNumeric];
+    return `The event will be held on ${eventDay}`
+
+}
+
+console.log(getEventWeekDay(2));
