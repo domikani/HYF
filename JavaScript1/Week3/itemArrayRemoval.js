47ea905d10b1a0d7fb8f14c0bdccfb04f1519db1
domna <domnakanari@gmail.com>
2022-01-06 20:56:25 +0000

    initial creation of exercise used methods slice, filter needed methods slice

--- JavaScript1/Week3/itemArrayRemoval.js
+++ JavaScript1/Week3/itemArrayRemoval.js
@@ -1,1 +1,26 @@
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  
+  // Write some code here
+  for(let name=0; name<names.length; name++){
+      if (names[name] == nameToRemove){
+          names.splice(name,1);
+      }
+  }
+  
+  
+  // Code done
+  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
 
+  //Alternative Do it with FIlter
+  let newNames = names.filter(name => name != nameToRemove);
+  console.log(newNames); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
