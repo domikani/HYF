79c3c18290cb1686f2987fcfa68a25fd32244559
domna <domnakanari@gmail.com>
2022-01-06 21:06:53 +0000

    exercise structure

---
+++ JavaScript1/Week3/beThere.js
@@ -0,0 +1,15 @@
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+  };
+
+  const calculateTime = (obj) =>{
+      
+  }
+  
+  const travelTime = calculateTime(travelInformation);
+  console.log(travelTime); // 8 hours and 38 minutes
+
+  //Write
+  // a function where you speficy your speed in km/h and 
+  //how far you have to go in km. The function has to return the time it will take to arrive at your destination. The time should be formatted like this: 3 hours and 34 minutes.
